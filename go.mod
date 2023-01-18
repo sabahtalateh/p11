73ae9ba252bc4ec0f10443a94c1040cc215ddb26
sabahtalateh <sabahtalateh@gmail.com>
2023-01-18 16:47:32 +0300

    wip

--- go.mod
+++ go.mod
@@ -1,1 +1,7 @@
 module github.com/sabahtalateh/p1/p11
+
+go 1.19
+
+require github.com/sabahtalateh/p1/p11/p12 v0.0.0
+
+replace github.com/sabahtalateh/p1/p11/p12 v0.0.0 => /Users/kravtsov777/Code/go/src/github.com/sabahtalateh/wireex/p12
--- p11.go
+++ p11.go
@@ -1,7 +1,20 @@
-package p11
+package main
+
+import (
+	"github.com/sabahtalateh/p1/p11/p12"
+	"github.com/sabahtalateh/p1/p11/p12/p13"
+)
 
 type P11 struct {
 }
 
 type P111 struct {
+}
+
+func main() {
+	p := p12.P12{}
+	println(&p)
+
+	pp := p13.P13{}
+	println(&pp)
 }
